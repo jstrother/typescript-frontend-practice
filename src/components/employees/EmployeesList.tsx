30d0862462e2fd4a27c6dda1e7d3e76704381f49
Jim <jim.strother@gmail.com>
2023-09-15 15:02:55 -0500

    Added Employee model and component, also EmployeeList component

---
+++ src/components/employees/EmployeesList.tsx
@@ -0,0 +1,30 @@
+import { Employee } from "../../model/Employee.model";
+
+interface EmployeesListProps {
+  employees: Employee[]
+}
+
+export function EmployeesList(props: EmployeesListProps) {
+  const renderedEmployees = props.employees.map((employee, index) => {
+    return (
+      <tr key={index}>
+        <td>{employee.name}</td>
+        <td>{employee.salary}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Salary</th>
+        </tr>
+      </thead>
+      <tbody>
+        {renderedEmployees}
+      </tbody>
+    </table>
+  );
+}
---
+++ src/services/DataService.ts
@@ -0,0 +1,27 @@
+import { Employee } from "../model/Employee.model";
+
+export async function getEmployees(): Promise<Employee[]> {
+  return [
+    {
+      name: 'Jim',
+      employedAt: new Date(),
+      id: '123',
+      position: 'Engineer',
+      salary: 100000
+    },
+    {
+      name: 'Deb',
+      employedAt: new Date(),
+      id: '456',
+      position: 'Manager',
+      salary: 100000
+    },
+    {
+      name: 'Oscar',
+      employedAt: new Date(),
+      id: '789',
+      position: 'HR',
+      salary: 100000
+    }
+  ];
+}
